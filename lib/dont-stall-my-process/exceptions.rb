c8f2a1e0c09bf2c96aafe0e70826bfb850efcad0
Malte Rohde <malte.rohde@flavoursys.com>
2014-09-09 14:47:36 +0200

    Make the exceptions actually inherit the base exception.

--- lib/dont-stall-my-process/exceptions.rb
+++ lib/dont-stall-my-process/exceptions.rb
@@ -2,13 +2,13 @@ module DontStallMyProcess
   DontStallMyProcessError = Class.new(StandardError)
 
   # This exception is raised when the watchdog bites.
-  TimeoutExceeded = Class.new(StandardError)
+  TimeoutExceeded = Class.new(DontStallMyProcessError)
 
   # This exception is raised when the subprocess could not be created, or
   # its initialization failed.
-  SubprocessInitializationFailed = Class.new(StandardError)
+  SubprocessInitializationFailed = Class.new(DontStallMyProcessError)
 
   # This exception is raised when a forbidden Kernel method is called.
   # These methods do not get forwarded over the DRb.
-  KernelMethodCalled = Class.new(StandardError)
+  KernelMethodCalled = Class.new(DontStallMyProcessError)
 end
